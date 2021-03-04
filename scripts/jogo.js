7c9c65c371ef62a52bc82a9b411b08b85098e22a
lucasAlmeidaSilveira <lucas.asilveira.sh@gmail.com>
2021-03-04 19:49:47 -0300

    Update cronometro

--- scripts/jogo.js
+++ scripts/jogo.js
@@ -3,6 +3,8 @@ const dados = {
     altura: 0,
     posicaoX: 0,
     posicaoY: 0,
+    vidas: 1,
+    tempo: 10,
 
     ajustaTamanhoPalcoJogo() {
         this.altura = window.innerHeight;
@@ -19,50 +21,67 @@ const dados = {
 
 const DOM = {
     createImg() {
-        if(document.getElementById('mosquito')) {
-            document.getElementById('mosquito').remove()
+        if (document.getElementById("box-mosquito")) {
+            document.getElementById("box-mosquito").remove();
+            if (dados.vidas > 3) {
+                window.location.href = "fim_de_jogo.html";
+            } else {
+                document.getElementById("v" + dados.vidas).src =
+                    "./assets/img/coracao_vazio.png";
+                dados.vidas++;
+            }
         }
 
-        posicaoX = dados.posicaoX < 0 ? 0 : dados.posicaoX
-        posicaoY = dados.posicaoY < 0 ? 0 : dados.posicaoY
+        posicaoX = dados.posicaoX < 0 ? 0 : dados.posicaoX;
+        posicaoY = dados.posicaoY < 0 ? 0 : dados.posicaoY;
 
-        const div = document.createElement('div')
+        const div = document.createElement("div");
+        div.id = "box-mosquito";
         div.innerHTML = `
-        <img id="mosquito" class="${this.tamanhoRandom()} ${this.ladoRandom()}" style="left:${posicaoX}px; top:${posicaoY}px" src="./assets/img/mosca.png">
-        `
-        document.body.appendChild(div)
+        <img onclick="document.getElementById('box-mosquito').remove()" class="${this.tamanhoRandom()} ${this.ladoRandom()}" style="left:${posicaoX}px; top:${posicaoY}px" src="./assets/img/mosca.png">
+        `;
+        document.body.appendChild(div);
     },
 
-    tamanhoRandom(){
-        var classe = Math.floor(Math.random() * 3)
-        switch(classe){
+    tamanhoRandom() {
+        const classe = Math.floor(Math.random() * 3);
+        switch (classe) {
             case 0:
-                return 'mosquito1'
+                return "mosquito1";
             case 1:
-                return 'mosquito2'
+                return "mosquito2";
             case 2:
-                return 'mosquito3'
+                return "mosquito3";
         }
     },
 
-    ladoRandom(){
-        var classe = Math.floor(Math.random() * 2)
-        if(classe == 1){
-            return 'ladoB'
+    ladoRandom() {
+        const classe = Math.floor(Math.random() * 2);
+        if (classe == 1) {
+            return "ladoB";
         }
     },
-    
 };
 
 const jogo = {
     init() {
-        setInterval(()=>{
+        const criarMosquito = setInterval(() => {
             dados.ajustaTamanhoPalcoJogo();
             dados.posicaoXY();
-            DOM.createImg()
-        }, 1000)
-
+            DOM.createImg();
+        }, 2000);
+        this.cronometro(criarMosquito);
+    },
+    cronometro(criarMosquito) {
+        let cronometro = setInterval(() => {
+            dados.tempo -= 1;
+            if (dados.tempo < 0) {
+                clearInterval(cronometro)
+                clearInterval(criarMosquito)
+                alert("Vitória");
+            } else {
+                document.getElementById("cronometro").innerHTML = dados.tempo;
+            }
+        }, 1000);
     },
 };
-
-
